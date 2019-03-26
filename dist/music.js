19d90f3fb3f50fbff0a2e452f7250174a0e271d9
huazhongge <339915764@qq.com>
2019-03-26 08:50:21 +0800

    Site updated: 2019-03-26 08:50:19

--- dist/music.js
+++ dist/music.js
@@ -1,5 +1,5 @@
 const ap = new APlayer({
-    container: document.getElementById('player'),
+    container: document.getElementById('aplayer'),
     listFolded: false,//列表默认折叠
     listMaxHeight: 90,//列表最大高度
     lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
